8f5bbc1143b2e885ddf6adca24954b3945297bd3
Marcin Erdmann <erdi84@gmail.com>
2012-09-16 13:21:00 +0100

    Simplified functional tests by using gaelyk-functional-test library

--- src/functionalTest/groovy/PostEditSpec.groovy
+++ src/functionalTest/groovy/PostEditSpec.groovy
@@ -1,49 +1,27 @@
 import geb.spock.GebSpec
-import page.LoginPage
-import groovyx.remote.client.RemoteControl
-import groovyx.remote.transport.http.HttpTransport
-import spock.lang.Shared
-import com.google.appengine.api.datastore.Entity
+import groovyx.gaelyk.functional.geb.DataSetupCategory
+import groovyx.gaelyk.functional.geb.LoginCategory
+import groovyx.gaelyk.functional.spock.ModifiesDatastore
 import page.PostEditPage
 
+@ModifiesDatastore
+@Mixin(LoginCategory)
+@Mixin(DataSetupCategory)
 class PostEditSpec extends GebSpec {
-
-	@Shared def remote = new RemoteControl(new HttpTransport("http://localhost:8080/remote-control"))
-
-	def setupSpec() {
-		remote.useNullIfResultWasUnserializable = true
-	}
-
-	def cleanup() {
-		remote.exec { datastore.iterate { select keys }.each { it.delete() } }
-	}
-
-	void 'some basics'() {
+	void 'post contents are displayed as markdown'() {
 		given:
-		remote.exec {
-			new Entity('posts', 'test-post').with {
+		setupData {
+			posts('test-post') {
 				markdown = 'Hello **World**!'
 				content = '<p>Hello <strong>World</strong>!</p>'
 				created = new Date()
-				save()
 			}
 		}
 
-		and:
-		adminIsLoggedIn()
-
 		when:
-		to PostEditPage, 'test-post'
+		loginTo 'admin@proxerd.pl', true, PostEditPage, 'test-post'
 
 		then:
 		postContents == 'Hello **World**!'
 	}
-
-	private void adminIsLoggedIn() {
-		to LoginPage
-		asAdmin = true
-		login.click()
-	}
-
-
 }
